7b740e4f230908c6c086811d543a1020a07fedca
jpb <jakub.baranski.33@gmail.com>
2024-06-07 22:19:59 -0700

    Make the values of Card dynamic

--- Ivirius.UI.Controls/Card.xaml.cs
+++ Ivirius.UI.Controls/Card.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,6 +24,105 @@ namespace Ivirius.UI.Controls
         public Card()
         {
             this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+        "Title", // The name of the property
+        typeof(string), // The type of the property
+        typeof(Card), // The type of the owner class
+        new PropertyMetadata("Title", TitleChanged) // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The title of the Card")]
+        public string Title
+        {
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+
+        private static void TitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Card)d).DetectTitleChange();
+        }
+
+        public void DetectTitleChange()
+        {
+            TitleTextBlock.Text = Title;
+        }
+
+        public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(
+        "Content", // The name of the property
+        typeof(string), // The type of the property
+        typeof(Card), // The type of the owner class
+        new PropertyMetadata("Content", ContentChanged) // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The content of the Card")]
+        public string Content
+        {
+            get
+            {
+                return (string)GetValue(ContentProperty);
+            }
+            set
+            {
+                SetValue(ContentProperty, value);
+            }
+        }
+
+        private static void ContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Card)d).DetectContentChange();
+        }
+
+        public void DetectContentChange()
+        {
+            TitleTextBlock.Text = Title;
+        }
+
+        public static readonly DependencyProperty SubtitleProperty =
+        DependencyProperty.Register(
+        "Subtitle", // The name of the property
+        typeof(string), // The type of the property
+        typeof(Card), // The type of the owner class
+        new PropertyMetadata("Subtitle", SubtitleChanged) // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The subtitle of the Card")]
+        public string Subtitle
+        {
+            get
+            {
+                return (string)GetValue(SubtitleProperty);
+            }
+            set
+            {
+                SetValue(SubtitleProperty, value);
+            }
+        }
+
+        private static void SubtitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Card)d).DetectSubtitleChange();
+        }
+
+        public void DetectSubtitleChange()
+        {
+            SubtitleTextBlock.Text = Subtitle;
         }
     }
 }
