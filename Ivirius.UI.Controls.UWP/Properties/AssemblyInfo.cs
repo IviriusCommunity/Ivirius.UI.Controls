5a1fc08c19f4ae9c87b4015ebbe6607f9fcb94a5
jpb <jakub.baranski.33@gmail.com>
2024-06-17 16:36:57 +0100

    Updates to UWP library

--- Ivirius.UI.Controls.UWP/Properties/AssemblyInfo.cs
+++ Ivirius.UI.Controls.UWP/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Ivirius.UI.Controls.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("Ivirius")]
 [assembly: AssemblyProduct("Ivirius.UI.Controls.UWP")]
-[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyCopyright("Copyright © Ivirius 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
 [assembly: ComVisible(false)]
