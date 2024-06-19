6f914072921dd29134318fdc4678af40fd21c2ab
jpb <jakub.baranski.33@gmail.com>
2024-06-19 08:06:25 +0100

    Revert "Remove old UWP port"
    This reverts commit 1b9615dc9ebb0a3ac85bdc6163524e8d02b0d0ee.

---
+++ Ivirius.UI.Controls.UWP/CommandLinkAccent.xaml.cs
@@ -0,0 +1,95 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Ivirius.UI.Controls
+{
+    public sealed partial class CommandLinkAccent : UserControl
+    {
+        public CommandLinkAccent()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+        "Title", // The name of the property
+        typeof(string), // The type of the property
+        typeof(CommandLinkAccent), // The type of the owner class
+        new PropertyMetadata("Title", TitleChanged) // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The title of the CommandLink")]
+        public string Title
+        {
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+
+        private static void TitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((CommandLinkAccent)d).DetectTitleChange();
+        }
+
+        public void DetectTitleChange()
+        {
+            TitleTextBlock.Text = Title;
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+        DependencyProperty.Register(
+        "Description", // The name of the property
+        typeof(string), // The type of the property
+        typeof(CommandLinkAccent), // The type of the owner class
+        new PropertyMetadata("Description", DescriptionChanged) // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The description of the CommandLink")]
+        public string Description
+        {
+            get
+            {
+                return (string)GetValue(DescriptionProperty);
+            }
+            set
+            {
+                SetValue(DescriptionProperty, value);
+            }
+        }
+
+        private static void DescriptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((CommandLinkAccent)d).DetectDescriptionChange();
+        }
+
+        public void DetectDescriptionChange()
+        {
+            DescriptionTextBlock.Text = Description;
+        }
+    }
+}
