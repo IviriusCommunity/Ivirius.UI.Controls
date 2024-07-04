2a9eb837187a416b5782c594d96abb85c71e58b9
jpb <jakub.baranski.33@gmail.com>
2024-07-04 16:19:30 +0100

    Rebase ChatBubble

--- Ivirius.UI.Controls/ChatBubble.cs
+++ Ivirius.UI.Controls/ChatBubble.cs
@@ -1,29 +1,25 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Ivirius.UI.Controls
 {
-    public sealed partial class ChatBubble : UserControl
+    public sealed class ChatBubble : Control
     {
         public ChatBubble()
         {
-            this.InitializeComponent();
+            this.DefaultStyleKey = typeof(ChatBubble);
         }
 
         public static readonly DependencyProperty TextProperty =
@@ -31,7 +27,7 @@ namespace Ivirius.UI.Controls
         "Text", // The name of the property
         typeof(string), // The type of the property
         typeof(ChatBubble), // The type of the owner class
-        new PropertyMetadata("Text", TextChanged) // Default value
+        new PropertyMetadata("Text") // Default value
         );
 
         [Browsable(true)]
@@ -47,16 +43,6 @@ namespace Ivirius.UI.Controls
             {
                 SetValue(TextProperty, value);
             }
-        }
-
-        private static void TextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            ((ChatBubble)d).DetectTextChange();
-        }
-
-        public void DetectTextChange()
-        {
-            BubbleText.Text = Text;
         }
     }
 }
