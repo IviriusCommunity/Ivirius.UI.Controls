05f7908e35235999918cd5a931e8c02948eaf516
jpb <jakub.baranski.33@gmail.com>
2024-07-16 18:57:54 +0100

    Allow ImageFrame to store content

--- Ivirius.UI.Controls/ImageFrame.cs
+++ Ivirius.UI.Controls/ImageFrame.cs
@@ -6,6 +6,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 
@@ -19,6 +20,23 @@ namespace Ivirius.UI.Controls
         public ImageFrame()
         {
             this.DefaultStyleKey = typeof(ImageFrame);
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register(
+        "Source", // The name of the property
+        typeof(string), // The type of the property
+        typeof(ImageFrame), // The type of the owner class
+        new PropertyMetadata("Source") // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The source of the content of the ImageFrame")]
+        public string Source
+        {
+            get { return (string)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
         }
     }
 }
